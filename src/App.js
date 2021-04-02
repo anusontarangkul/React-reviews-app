131fa48a9fcd12690bd6dab11e1f2d588f95c5c5
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-04-01 21:35:25 -0700

    add styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <main>
+
+      <section className="title">
+        <h2>Our Reviews</h2>
+        <div className="underline"></div>
+      </section>
+    </main>
   );
 }
 
